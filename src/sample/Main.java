674ab559185028e37aba612b6ed12c24325aeda0
btan7 <btan7@gatech.edu>
2016-09-22 01:49:21 -0400

    Implemented login, unimplemented logout button

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,20 +3,18 @@ package sample;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
+import java.util.HashMap;
+
 public class Main extends Application {
-    Stage stage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -43,9 +41,16 @@ public class Main extends Application {
             loginPane.getColumnConstraints().add(column);
         }
 
+        GridPane loggedinPane = new GridPane();
+        for (int i = 0; i < 3; i++) {
+            ColumnConstraints column = new ColumnConstraints(100);
+            loggedinPane.getColumnConstraints().add(column);
+        }
+
         //Scene home = new Scene(gp, gp.getWidth(), gp.getHeight());
         Scene home = new Scene(gp);
         Scene loginScreen = new Scene(loginPane);
+        Scene in = new Scene(loggedinPane);
 
         Button login = new Button("Login");
         login.setMaxWidth(120);
@@ -78,23 +83,49 @@ public class Main extends Application {
         space.minHeightProperty().bind(label.heightProperty());
         Pane space2 = new Pane();
         space2.minHeightProperty().bind(label.heightProperty());
+        TextField userTF = new TextField();
+        PasswordField passF = new PasswordField();
+        Button letMeIn = new Button("Login");
         loginPane.add(loginTitle, 2, 0);
+        loginPane.add(goHome, 0, 0);
         loginPane.add(space, 1, 1);
         loginPane.add(new Text("Username: "), 1, 2);
-        loginPane.add(new TextField(), 2, 2);
+        loginPane.add(userTF, 2, 2);
         loginPane.add(new Text("Password: "), 1, 3);
-        loginPane.add(new PasswordField(), 2, 3);
-        loginPane.add(space2, 2, 4);
-        loginPane.add(goHome, 0, 0);
+        loginPane.add(passF, 2, 3);
+        loginPane.add(space2, 2, 5);
+        loginPane.add(letMeIn, 2, 4);
         for (int i = 0; i <= 4; i++) {
             RowConstraints row = new RowConstraints();
             ColumnConstraints col = new ColumnConstraints();
-            // Here we set the pref height of the row, but you could also use .setPercentHeight(double) if you don't know much space you will need for each label.
             row.setPrefHeight(50);
             col.setPrefWidth(50);
             loginPane.getRowConstraints().add(row);
             loginPane.getColumnConstraints().add(col);
         }
+
+        //Screen for authenticated user
+        Button logout = new Button("Logout");
+        loggedinPane.add(logout, 0, 0);
+
+        //User credential data
+        HashMap<String, String> users = new HashMap<String, String>();
+        //Hardcoded username and password for M4 checkpoint
+        users.put("user", "pass");
+
+        letMeIn.setOnAction(e-> {
+            String checkUser = userTF.getText();
+            String checkPW = passF.getText();
+            if (users.get(checkUser) != null && checkPW.equals(users.get(checkUser))) {
+                primaryStage.setScene(in);
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Invalid Login");
+                alert.setHeaderText("Username and/or Password Not Found");
+                alert.setContentText("Username and/or password was entered incorrectly.");
+                alert.showAndWait();
+            }
+        });
 
 //        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.setScene(home);

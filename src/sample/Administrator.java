f2720abb342ef0f48b71a654f83cf5e5141021e3
Vivienne Qie <qievi@gatech.edu>
2016-09-26 17:27:36 -0400

    Added user, manager, worker, and admin classes

---
+++ src/sample/Administrator.java
@@ -0,0 +1,78 @@
+package sample;
+
+public class Administrator {
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private int phoneNumber;
+    private String title;
+
+    public Administrator() {
+        title = "Administrator";
+    }
+
+    public Administrator(String username, String password, String email,
+                         String firstName, String lastName, int phoneNumber) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        title = "Administrator";
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}

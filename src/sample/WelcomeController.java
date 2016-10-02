e27bda63e0c6c26a55b3d513139a85f85ceda21c
as1216 <arshiya.singh16@gmail.com>
2016-10-02 12:35:37 -0400

    hides welcome screen when register and login button is clicked

--- src/sample/WelcomeController.java
+++ src/sample/WelcomeController.java
@@ -1,6 +1,8 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class WelcomeController {
 
@@ -10,12 +12,23 @@ public class WelcomeController {
     public void setMainApp(MainFXApp mainFXApp) {
         mainFXApplication = mainFXApp;}
 
+    @FXML
+    public Button login;
 
     @FXML
-    public void onRegPressed() { mainFXApplication.goToRegistration(); }
+    public Button register;
+
+    @FXML
+    public void onRegPressed() {
+        Stage stage = (Stage) register.getScene().getWindow(); //cast as Stage
+        stage.close(); //close welcome screen
+        mainFXApplication.goToRegistration();
+    }
 
     @FXML
     public void onLoginPressed() {
+        Stage stage = (Stage) login.getScene().getWindow(); //cast as Stage
+        stage.close(); //close welcome screen
         mainFXApplication.goToLogin();
     }
 }

a52090caae63fa2ee3d89b76823c94d9d2ac9472
as1216 <arshiya.singh16@gmail.com>
2016-10-12 16:25:12 -0400

    added Availability Form UI; added fx:id and handle functions for submit and cancel buttons; todos assigned;

---
+++ src/sample/AvailabilitySubmissionForm.java
@@ -0,0 +1,68 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.fxml.*;
+import javafx.scene.control.*;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Archie on 10/12/2016.
+ */
+public class AvailabilitySubmissionForm {
+    @FXML
+    private BorderPane reportFormPane;
+
+    @FXML
+    private ComboBox typeBox;
+
+    @FXML
+    private ComboBox condBox;
+
+    //TODO: change this to coordinates from Google Maps for extra cred
+    @FXML
+    private TextField locationText;
+
+    @FXML
+    private Button cancelReport;
+
+    @FXML
+    private Button submitReport;
+
+    private Stage _dialogStage;
+
+    private MainFXApp mainFXApplication;
+
+    public void setMainApp(MainFXApp mainFXApp) { mainFXApplication = mainFXApp; }
+
+    public void setDialogStage(Stage dialogStage) {
+        _dialogStage = dialogStage;
+    }
+
+    @FXML
+    private void initialize() {
+        String[] waterTypes = {"Bottled", "Welled", "Steam", "Lake", "Spring", "Other"};
+        ObservableList typeList = FXCollections.observableArrayList(waterTypes);
+        typeBox.getItems().clear();
+        typeBox.setItems(typeList);
+
+        String[] condTypes = {"Waste", "Treatable - Muddy", "Treatable - Clear", "Potable"};
+        ObservableList condList = FXCollections.observableArrayList(condTypes);
+        condBox.getItems().clear();
+        condBox.setItems(condList);
+    }
+
+    @FXML
+    private void handleSubmission() {
+        //TODO: handle submission and verify fields here
+    }
+
+    @FXML
+    private void handleCancelReport() {
+        _dialogStage.close();
+        //TODO:set where this goes after report is canceled
+    }
+}
